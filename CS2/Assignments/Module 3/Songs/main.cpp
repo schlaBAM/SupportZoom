c9de4def436e84924a2ee55a0552d5ce50e600bf
schlaBAM <brett.morrison@shopify.com>
2019-11-29 00:37:10 -0800

    Added completed version of songs

--- CS2/Assignments/Module 3/Songs/main.cpp
+++ CS2/Assignments/Module 3/Songs/main.cpp
@@ -13,15 +13,15 @@ struct song {
     }
 };
 
+void addSongs(song**, int);
 void printLibrary(song**, int);
 song** songsFromYear(song **songs, int size, int year, int* resultSize);
 void deallocateMemory(song**, int);
-void addSongs(song**, int);
 
 int main(){
 
-    int LIBRARY_SIZE = 10;
-    song **songLibrary = new song*[LIBRARY_SIZE];
+    const int LIBRARY_SIZE = 10;
+    song **songLibrary = new song *[LIBRARY_SIZE];
 
     addSongs(songLibrary, LIBRARY_SIZE);
     printLibrary(songLibrary, LIBRARY_SIZE);
@@ -29,7 +29,6 @@ int main(){
     cout << "-----------" << endl;
 
     int resultSize = 0;
-
     song **resultArray = songsFromYear(songLibrary, LIBRARY_SIZE, 2014, &resultSize);
     printLibrary(resultArray, resultSize);
 //    printLibrary(songsFromYear(songLibrary, LIBRARY_SIZE, 2014, &resultSize), resultSize);
@@ -46,25 +45,27 @@ int main(){
 
     cout << "\n-----------" << endl;
 
+    resultSize = 0;
+    resultArray = songsFromYear(songLibrary, LIBRARY_SIZE, 1995, &resultSize);
+    printLibrary(resultArray, resultSize);
+    cout << "Clearing filtered song library.." << endl;
+    deallocateMemory(resultArray, resultSize);
+
+    cout << "\n-----------" << endl;
+
+    //test with 0 results available.
+    resultSize = 0;
+    resultArray = songsFromYear(songLibrary, LIBRARY_SIZE, 10000, &resultSize);
+    printLibrary(resultArray, resultSize);
+    cout << "Clearing filtered song library.." << endl;
+    deallocateMemory(resultArray, resultSize);
+
+    cout << "\n-----------" << endl;
+
     cout << "\nClearing main song library.." << endl;
     deallocateMemory(songLibrary, LIBRARY_SIZE);
-//    printLibrary(songLibrary, LIBRARY_SIZE);
 
     return 0;
-}
-
-void printLibrary(song **library, int size){
-
-    cout << "\nAvailable songs in this library: \n" << endl;
-
-    for(int i = 0; i < size; i++){
-        if(library[i] != nullptr){
-            cout << "\t" + library[i]->songToString() << endl;
-        } else {
-            cout << "Element at position " + to_string(i) + " is null" << endl;
-        }
-    }
-    cout << endl;
 }
 
 void addSongs(song **songLibrary, int size) {
@@ -72,8 +73,6 @@ void addSongs(song **songLibrary, int size) {
     for(int i = 0; i < size; i++){
         songLibrary[i] = new song;
     }
-
-    //think this is what's being asked for.
 
     (*songLibrary[0]) = {"Put It On", "Big L", 1995};
     (*songLibrary[1]) = {"Under Pressure", "Logic", 2014};
@@ -88,29 +87,53 @@ void addSongs(song **songLibrary, int size) {
 
 }
 
+void printLibrary(song **library, int size){
+
+    if(size > 0) {
+        cout << "\nAvailable songs in this library: \n" << endl;
+
+        for (int i = 0; i < size; i++) {
+            if (library[i] != nullptr) {
+                cout << "\t" + library[i]->songToString() << endl;
+            } else {
+                cout << "Element at position " + to_string(i) + " is null" << endl;
+            }
+        }
+    } else{
+        cout << "\nNo results found from year requested." << endl;
+    }
+
+    cout << endl;
+}
+
 song** songsFromYear(song **songs, int size, int year, int* resultSize){
 
-    // resultSize starts pointing to 0
-    // in loop, if song.year == year then place into resArray[resultSize++]
-    // after loop return resArray
-
-    song **resultArray = new song *[size];
-
-    for(int i = 0; i < size; i++){
-        if(songs[i]->creationYear == year){
-            *resultSize = *resultSize + 1;
-            resultArray[*resultSize - 1] = new song;
-            (*resultArray[*resultSize - 1]) = {songs[i]->title, songs[i]->artist, songs[i]->creationYear};
+    //get count of matches for precise array size
+    int count = 0;
+    for(int i = 0; i < size; i++) {
+        if (songs[i]->creationYear == year) {
+            count++;
         }
     }
 
+    song **resultArray = new song *[count];
+
+    // resultSize starts pointing to 0
+    // in loop, if song.year == year then place into resArray[resultSize++]
+
+    for(int i = 0; i < size; i++){
+        if(songs[i]->creationYear == year){
+            resultArray[*resultSize] = new song;
+            (*resultArray[*resultSize]) = {songs[i]->title, songs[i]->artist, songs[i]->creationYear};
+            (*resultSize)++;
+        }
+    }
+
+    //not sure how to use unique_ptr with double pointer to deallocate here.
     return resultArray;
 }
 
 void deallocateMemory(song **library, int size){
-
-    //TODO Be sure to properly deallocate all dynamically allocated memory. For every
-    // call to new, there should be a corresponding delete or delete[].
 
     for(int i = 0; i < size; i++){
         if(library[i] != nullptr)
