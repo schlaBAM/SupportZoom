84501336aa07d34da2a3dae1f3ca2cc30fa09e8b
schlaBAM <brett.morrison@shopify.com>
2019-12-02 16:29:18 -0800

    Adding Java -> C++ assignment, trying to fix file structure between two IDEs

---
+++ CS2/Assignments/Module 4/TranslatingJavaToC++/main.cpp
@@ -0,0 +1,60 @@
+/*
+ * Translating:
+ * public class Question2
+{
+  public static float getAverageScore(int[] scoreArray)
+  {
+    float total = 0;
+    for (int i=0; i < scoreArray.length; i++)
+    {
+      total += scoreArray[i];
+    }
+    return total / scoreArray.length;
+  }
+
+  public static void main(String[] args)
+  {
+    int[] scores = new int[10];
+
+    for (int i=0; i < scores.length; i +=2 )
+    {
+      scores[i] = i;
+    }
+
+    System.out.println("Average score: " + getAverageScore(scores));
+  }
+}
+
+ */
+
+
+#include <iostream>
+using namespace std;
+
+float getAverageScore(int, int);
+
+int main(){
+    const int ARRAY_SIZE = 10;
+
+    int scores[ARRAY_SIZE];
+
+    for(int i = 0; i < ARRAY_SIZE; i += 2){
+        scores[i] = new int;
+        scores[i] = i;
+    }
+
+    cout << "Average score: " << getAverageScore(scores, ARRAY_SIZE) << endl;
+
+    return 0;
+}
+
+float getAverageScore(int* scoreArray, int size){
+    float total = 0;
+    for(int i = 0; i < size; i++){
+        if(scoreArray[i] != nullptr) {
+            total += scoreArray[i];
+        }
+    }
+
+    return total / size;
+}
